bb13ff7e280fb6f4994caab3111560f7bdff3f18
Andy Bird <abird@asascience.com>
2014-01-31 09:47:34 -0500

    update coverageFDW to use cov_path param, also uses correct date time

--- python/multicorn/covfdw.py
+++ python/multicorn/covfdw.py
@@ -3,13 +3,29 @@ An ION Coverage Foreign Data Wrapper
 
 """
 
+import sys
+import math
+import numpy as np
+import numpy
+import string
+
+from numpy.random import random
+
+import simplejson
+from gevent import server
+from gevent.monkey import patch_all; patch_all()
+from pyon.util.breakpoint import breakpoint
+from coverage_model import SimplexCoverage, QuantityType, ArrayType, ConstantType, CategoryType
+import gevent
+
 from multicorn import ForeignDataWrapper
 from .utils import log_to_postgres
-import random
 from logging import WARNING
-from logging import INFO
-import os, sys, inspect
-from coverage_model import SimplexCoverage
+
+import random
+from random import randrange
+
+import datetime
 
 class CovFdw(ForeignDataWrapper):
     """A foreign data wrapper for accessing an ION coverage data model.
@@ -20,34 +36,50 @@ class CovFdw(ForeignDataWrapper):
     def __init__(self, fdw_options, fdw_columns):
         super(CovFdw, self).__init__(fdw_options, fdw_columns)
         self.k = fdw_options["k"]
+        self.cov_path = fdw_options["cov_path"]
         self.columns = fdw_columns
 
     def execute(self, quals, columns):
+        log_to_postgres("LOADING Coverage", WARNING)
+        log_to_postgres("coverage path:"+self.cov_path, WARNING)
+        cov = SimplexCoverage.load(self.cov_path)
+        log_to_postgres("DataFields:"+str(columns), WARNING)
+        log_to_postgres("---------------------", WARNING)
 
-        log_to_postgres("INFO:"+str(columns), WARNING)
+        paramTemp = cov.get_parameter_values('temp').tolist()
+        paramCond = cov.get_parameter_values('conductivity').tolist()
+        paramTime = cov.get_parameter_values('time').tolist()
+        paramLat = cov.get_parameter_values('lat').tolist()
+        paramLon = cov.get_parameter_values('lon').tolist()
 
-        for x in range(0, 10):
+        baseDateTime = datetime.datetime(2011,2,11,1,1,1)
+
+        for i in range(0,len(paramTime)):
             line=[]
+            
+            dt = baseDateTime + datetime.timedelta(0,paramTime[i])
+            s = dt.strftime("%Y-%m-%d %H:%M:%S")
+
             for f in self.columns:
-                v = random.randrange(1, 100, 1)
                 if (f == "lat"):
-                    line.append("25.5")   
+                    line.append(paramLat[i]+(0.01*i))   
                 elif (f == "lon"):     
-                    line.append("11.1")
+                    line.append(paramLon[i]+(0.01*i))
                 elif (f == "dataset_id"):      
                     line.append("d010")
                 elif (f == "cond"):
-                    line.append(str(v))
+                    line.append(paramCond[i])
                 elif (f == "temp"):          
-                    line.append(str(v))
+                    line.append(paramTemp[i])
+                elif (f == "time"):          
+                    line.append(s) 
                 elif (f == "geom"):          
                    line.append("0101000020E610000000000000000039400000000000002640")
-                else:
-                    line.append("0:"+f)
+                
 
 
             if len(line) > len(self.columns):
-                            log_to_postgres("There are more columns than "
+                log_to_postgres("There are more columns than "
                                             "defined in the table", WARNING)
             elif len(line) < len(self.columns):
                 log_to_postgres("There are less columns than "
